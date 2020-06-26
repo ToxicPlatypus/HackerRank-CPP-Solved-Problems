5196c5d3049fb0125fa317b3b66dfa9c9627b249
Mohammad Rafid Rabbi <rafidrabbi123@gmail.com>
2020-06-26 21:27:29 +0600

    Basic Data Types

---
+++ Basic Data Types.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    long int b;
+    char s;
+    float x;
+    double y;
+
+    scanf("%d %ld %c %f %lf", &a, &b, &s, &x, &y);
+    printf("%d\n%ld\n%c\n%f\n%lf\n", a, b, s, x, y);
+    return 0;
+}
